6c977963c070eae92847901cfadb0b53decb1f8e
Willpatbarr <145700970+Willpatbarr@users.noreply.github.com>
2025-05-14 23:24:30 -0600

    completed NPS 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+document.title = parkData.fullName;
+
+document.querySelector(".hero-banner img").src = parkData.images[0].url;
+
+const heroInfo = document.querySelector(".hero-banner__info");
+heroInfo.innerHTML = `
+  <a href="/" class="hero-banner__title">${parkData.name}</a>
+  <p class="hero-banner__subtitle">
+    <span>${parkData.designation}</span>
+    <span>${parkData.states}</span>
+  </p>
+`;
+
